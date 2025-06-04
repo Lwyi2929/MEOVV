43a2807b83e31db0de0b2a11d5b3271286a19521
Lwyi2929 <lwyi2929@gmail.com>
2025-06-05 00:56:29 +0800

    Update 2_⛰️觀光帶來的影響.py

--- pages/2_⛰️觀光帶來的影響.py
+++ pages/2_⛰️觀光帶來的影響.py
@@ -6,4 +6,4 @@ st.subheader("""
 1985年，隸屬於退輔會的清境國民賓館落成，921地震後帶動了觀光業，清境的民宿從十家變一百多家，遊客量也大增，不少業者為了增加房間數，違法擴建。民宿爭奇鬥豔，違法亂象與坡地安全，造成非都市土地使用失控。
 """)
 # Display the image
-st.image("tourist.png", caption="Annual tourist visits to Qingjing Farm",use_container_width=True)
+st.image("tourists.png", caption="Annual tourist visits to Qingjing Farm",use_container_width=True)
