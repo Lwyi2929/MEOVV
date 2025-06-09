0e43a7c70a891f130dc56ebd6afb48f6fa47a49c
Lwyi2929 <lwyi2929@gmail.com>
2025-06-09 19:18:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,42 +29,21 @@ st.markdown(markdown)
 st.image("sheep.png", caption="""青青草原""",use_container_width=True)
 
 
-#st.title("選擇日期區間")
 
-
-# 初始化 session_state
-#if 'start_date' not in st.session_state:
-#    st.session_state['start_date'] = date(2024, 1, 1)
-#if 'end_date' not in st.session_state:
-#    st.session_state['end_date'] = date.today()
-
-#st.session_state['start_date'] = date(2024, 1, 1)
-#st.session_state['end_date'] = date.today()
-
-
-# 日期選擇器
-#start_date = st.date_input(label = "選擇起始日期", value = st.session_state['start_date'], min_value = date(2018, 1, 1), max_value = date.today())
-#end_date = st.date_input(label = "選擇結束日期", value = st.session_state['end_date'], min_value = start_date, max_value = date.today())
-
-# 儲存使用者選擇
-#st.session_state['start_date'] = start_date
-#st.session_state['end_date'] = end_date
-
-#st.success(f"目前選擇的日期區間為：{start_date} 到 {end_date}")
 
 
 st.title("利用擴充器示範")
 markdown = """
-2016年~2024年的衛星影像時序動畫
+衛星影像時序動畫
 """
 st.markdown(markdown)
 
-with st.expander("播放真色影像mp4檔(landsat)"):
+with st.expander("播放真色影像mp4檔(landsat)1984年~2024年"):
     video_file = open("qingjing_true (1).mp4", "rb")  # "rb"指的是讀取二進位檔案（圖片、影片）
     video_bytes = video_file.read()
     st.video(video_bytes)
 
-with st.expander("播放假色影像mp4檔(sentinel2) bands=['B8', 'B4', 'B3']"):
+with st.expander("播放假色影像mp4檔(sentinel2) bands=['B8', 'B4', 'B3']2016年~2024年"):
     video_file = open("false_qingjing.mp4", "rb")  # "rb"指的是讀取二進位檔案（圖片、影片）
     video_bytes = video_file.read()
     st.video(video_bytes)
